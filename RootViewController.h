3ebd152d6de4357178af666f171260c87cd5ba46
Du Zixi <duzixi@gmail.com>
2014-01-09 15:12:18 +0800

    Create RootViewController.h

---
+++ RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  MyColors
+//
+//  Created by 杜子兮 on 14-1-7.
+//  Copyright (c) 2014年 莲兮奈若何. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
