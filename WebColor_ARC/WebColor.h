878c0ccd248b41348acb08776c49b8cd62a9cdab
Du Zixi <duzixi@gmail.com>
2014-08-23 14:05:11 +0800

    Update WebColor.h

--- WebColor_ARC/WebColor.h
+++ WebColor_ARC/WebColor.h
@@ -2,11 +2,29 @@
 //  WebColor.h
 //  (Please use in ARC mode)
 //
-//  Created by 杜子兮 on 14-1-7.
-//  Copyright (c) 2014年 莲兮奈若何. All rights reserved.
+//  Created by 杜子兮(duzixi) on 14-01-07.
+//  Edited  by 杜子兮(duzixi) on 14-08-23.
+//  Copyright (c) 2014年 duzixi.com All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+
+@interface ColorModel : NSObject
+
+@property (nonatomic,retain) NSString * name;
+@property (nonatomic,retain) NSString * nameCN;
+@property (nonatomic,assign) CGFloat r;
+@property (nonatomic,assign) CGFloat g;
+@property (nonatomic,assign) CGFloat b;
+
+- (id)initWithName:(NSString *) name nameCN:(NSString *)nameCN r:(CGFloat ) r g:(CGFloat )g b:(CGFloat )b;
+
++ (ColorModel *) colorModelWithName:(NSString *) name nameCN:(NSString *)nameCN r:(CGFloat ) r g:(CGFloat )g b:(CGFloat )b;
+
+- (UIColor *) getColor;
+
+@end
+
 
 @interface WebColor : NSObject
 
